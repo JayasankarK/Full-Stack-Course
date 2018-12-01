e96e884f56a0282dd6a980c0ec7af9f7cd82e745
JayasankarK <jayasankar.sr8@gmail.com>
2018-12-01 13:08:10 +0530

    Javascript learning start

---
+++ Javascript/ifElse.js
@@ -0,0 +1,9 @@
+var x=30;
+if(x<20)
+{
+    console.log("value is less than 20");
+}
+else
+{
+    console.log("value is greater than 20");
+}
---
+++ Javascript/script.js
@@ -0,0 +1,10 @@
+function add(a,b){
+    var c= a+b;
+    return c;
+}
+console.log(add(20,60));
+function exponential(a,b){
+    var m=a**b;
+    return m;
+}
+console.log(exponential(9,2));
